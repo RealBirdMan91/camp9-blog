8ee23997a6939b2133662e5b3a2a13e4fd76d48c
Julian Vogel <birdman.coding.1991@gmail.com>
2023-04-11 15:12:07 +0200

    add async

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface BlogPost {
+  id: number;
+  title: string;
+  content: string;
+}
 
 function Blog() {
+  const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const { data } = await axios.get<BlogPost[]>(
+        "http://localhost:3000/posts"
+      );
+      setBlogPosts(data);
+    }
+    getPosts();
+  }, []);
+
   return (
     <div>
       <h1 className="text-4xl font-bold">Hello Blog</h1>
+      {blogPosts.map((post) => (
+        <section key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content.substring(0, 150)}...</p>
+        </section>
+      ))}
     </div>
   );
 }
